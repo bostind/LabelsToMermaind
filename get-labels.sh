0e8a4c1a2797742d0bc9f62ae1d8384442d15444
Bostind <Bostind@msn.com>
2024-09-28 10:58:12 +0800

    优化输出

--- get-labels.sh
+++ get-labels.sh
@@ -11,6 +11,7 @@ fi
 
 # 清空或创建输出文件
 echo > "$output_file"
+
 # 获取所有节点的标签并提取需要的字段
 if ! kubectl get nodes -o json | jq -r '.items[] | 
     {name: .metadata.name, 
@@ -19,8 +20,9 @@ if ! kubectl get nodes -o json | jq -r '.items[] |
      rack: .metadata.labels["topology.kubernetes.io/rack"], 
      hostname: .metadata.labels["kubernetes.io/hostname"]} | 
     select(.hostname != null or .zone != null or .rack != null or .region != null) | 
-    "\(.name), \(.region), \(.zone), \(.rack), \(.hostname)"' >> "$output_file"; then
+    "\(.name)  region=\(.region) zone=\(.zone)  rack=\(.rack)  hostname=\(.hostname)"' >> "$output_file"; then
     echo "Error: Failed to retrieve nodes or process data." >&2
 fi
+
 # 添加文件结束标记
-    echo "Output saved to $output_file"
+echo "Output saved to $output_file"
