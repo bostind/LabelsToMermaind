3fef636a520575f88b11d0c58c042e4e26e85f79
Bostind <Bostind@msn.com>
2024-09-29 11:18:43 +0800

    增加架构转标签

--- mermaindToLabels.sh
+++ mermaindToLabels.sh
@@ -1,7 +1,48 @@
 #!/bin/bash
 
+# 定义输入文件
+input_file="subgraph.txt"
+
+# 定义变量
+declare -a zones
+declare -a racks
+declare -A hosts
+
+# 从输入文件中提取区域、区域和机架信息
+while IFS= read -r line
+do
+    if [[ $line =~ region:(.+) ]]; then
+        # 提取区域名称
+        region=${BASH_REMATCH[1]}
+    elif [[ $line =~ zone:(.+) ]]; then
+        # 提取区域名称并添加到zones数组中
+        zone=${BASH_REMATCH[1]}
+        zones+=("$zone")
+    elif [[ $line =~ rack:(.+) ]]; then
+        # 提取机架名称并添加到racks数组中
+        rack=${BASH_REMATCH[1]}
+        racks+=("$rack")
+    elif [[ $line =~ hostname:(.+) ]]; then
+        hostname=${BASH_REMATCH[1]}
+        # 将主机名与相应的区域和机架信息保存到hosts数组中
+        hosts["$hostname"]="region=$region zone=${zones[-1]} rack=${racks[-1]} hostname=$hostname"
+    fi
+done < "$input_file"
+
+# 定义输出文件
+output_file="labelsoutput.txt"
+echo > $output_file
+# 输出结果到文件
+for host in "${!hosts[@]}"; do
+    echo "$host  ${hosts[$host]}" >> "$output_file"
+done
+# 去掉空行
+sed -i '/^$/d' "$output_file"
+# 添加文件结束标记
+echo "Output saved labels to $output_file"
+
 # 标签配置文件路径
-LABELS_FILE="k8s_node_labels_extracted.txt"
+LABELS_FILE=$output_file
 # 检查文件是否存在
 if [ ! -f "$LABELS_FILE" ]; then
     echo "标签配置文件 $LABELS_FILE 不存在"
@@ -15,7 +56,7 @@ while IFS= read -r line; do
     LABELS=$(echo "$line" | awk -F' ' '{$1=""; sub(/^ +/, ""); print}' ) 
 
     # 打标签
-    kubectl label nodes "$NODE" $LABELS --overwrite
+    kubectl label nodes "$NODE" $LABELS --overwrite 
     if [ $? -eq 0 ]; then
         echo "成功给节点 $NODE 添加标签: $LABELS"
     else
